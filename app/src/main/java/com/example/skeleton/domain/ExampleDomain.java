67dcd9175a15a398e0c947b0141d69316f96ac74
ChrisStedman <christof_423@hotmail.com>
2023-04-23 20:53:49 +1000

    Update ExampleDomain to specify Table and sequence created by init.sql

--- app/src/main/java/com/example/skeleton/domain/ExampleDomain.java
+++ app/src/main/java/com/example/skeleton/domain/ExampleDomain.java
@@ -2,20 +2,23 @@ package com.example.skeleton.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "Example")
+@Table(name = "example_table")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExampleDomain {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "example_sequence", sequenceName = "example_sequence", allocationSize = 1)
+    @GeneratedValue(generator = "example_sequence")
     private Long id;
     private String name;
 }
